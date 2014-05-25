273d29c20aedde932b81dd1504ab0ee2d0a8131d
Sempronius <jryanmason@gmail.com>
2014-05-25 14:15:51 -0500

    program assignement 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a matrix as the input x. In the end,
+## it will create a variable that is a list (containing the functions: set, get, setinverse, and getinverse)  
+##The files cacheSolve.R will take as an input, a variable created from makeCacheMatrix.R
+## in order to output the inverse of the matrix.
 
-## see comments in the program
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
-        set <- function(y) {
-                x <<- y # This is overriding the value of x previously , because we want to 'set' a new one!
-                i <<- NULL #Since we are setting a new variable, we need to reset i on line 7
+        set <- function(y) { ## creates a function
+                x <<- y ## y overrides x. 
+                i <<- NULL ## Null overrides i
         }
-        get <- function() {   # this line makes a FUNCTION out of "get", which prints x.
-                x
+        get <- function() { ## creates  a function
+                x ## displays value of x
         }
-        setinverse <- function(inverse) {  # setinverse is a function of the input "inverse". this function assigns "inverse" to "i"
-                i <<- inverse   #this input "inverse" is now going to OVERRIDE the i in line 7.
+        setinverse <- function(inverse) {
+                ## creates function
+                i <<- inverse ## value of inverse
+                ## overrides value of i.
         }
-        getinverse <- function() {  #this function simply prints the value of i
-                i
+        getinverse <- function() {
+                ## creates a function
+                i ## displays the value of i
         }
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
                 
 }
-## see comments in the program...
+      
+                
+}
+## This function takes an input x , which is the RESULT from the 'makeCacheMatrix.R' function.
 
 cacheSolve <- function(x, ...) {
-       cacheSolve <- function(x, ...) {
-        i <- x$getinverse() # x is a RESULT from the makeCacheMatrix.R function. The $ accesses the 'getinverse()' function... inside the function makeCacheMatrix.R !
-        if(!is.null(i)) { #this is a simply if statement to see if you have already calculated the inverse of x. If so, it just spits out i!
+        i <- x$getinverse()
+        ##The $ accesses the 'getinverse()' function, which is inside the function makeCacheMatrix.R !
+        if(!is.null(i)) { #this is a simply if statement to see if you have already calculated 
+                ##the inverse of x. If so, it just spits out i!
                 message("getting cached data")
                 return(i)
         }
-        data <- x$get() # So, this assigns the value of the get() function of x (a result from makeCacheMatrix.R) to data. Which is whatever you put in the original makeCacheMatrix(*HERE*)
+        data <- x$get() # So, this assigns the value of the 
+        ## get() function of x (a result from makeCacheMatrix.R) 
+        ## to data. Which is whatever you put in the original makeCacheMatrix(*HERE*)
         i <- solve(data, ...) #just calculates the inverse of data, and assigns it to i.
-        x$setinverse(i) # accesses the function setmean stored in x... and puts m into it. Remember: setmean(mean) { m <<- mean }.THis overrides m in the original x *result of makevector* 
+        x$setinverse(i) # accesses the function setmean stored in x... and puts m into it. 
+        ## Remember: setmean(mean) { m <<- mean }.
+        ##THis overrides m in the original x *result of makevector* 
         i
-}
+        }
